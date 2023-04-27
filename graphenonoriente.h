6cb57d48f48cec43661889e5c054d28465d3bc18
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-04-27 02:37:15 +0200

    Update graphenonoriente.h

--- graphenonoriente.h
+++ graphenonoriente.h
@@ -10,6 +10,7 @@ public:
     GrapheNonOriente(const vector<vector<Sommet>>& matrice, int n, int m);
     GrapheNonOriente(const vector<Sommet>& fs, const vector<int>& aps, int n, int m);
     GrapheNonOriente(const vector<Chainon>& lp, const vector<Chainon>& ls, int n, int m);
+    bool lireGraphe(std::string nomFic) override;
 
     void rang(vector<int>& rang) override;
     bool codagePrufer (vector <int> & prufer) override;
