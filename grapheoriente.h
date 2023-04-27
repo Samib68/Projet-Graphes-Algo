0aa0b8ee896fdf931d2d91db099fe086758d0c97
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-04-27 02:38:52 +0200

    Update grapheoriente.h

--- grapheoriente.h
+++ grapheoriente.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <stack>
 #include "graphe.h"
-
+#include<string>
 class GrapheOriente : public Graphe
 {
 public:
@@ -12,6 +12,7 @@ public:
     GrapheOriente(const vector<vector<Sommet>>& matrice, int n, int m);
     GrapheOriente(const vector<Sommet>& fs, const vector<int>& aps, int n, int m);
     GrapheOriente(const vector<Chainon>& lp, const vector<Chainon>& ls, int n, int m);
+    bool lireGraphe(std::string nomFic) override;
 
     void rang(vector<int>& rang) override;
     bool codagePrufer (vector <int> & prufer) override;
